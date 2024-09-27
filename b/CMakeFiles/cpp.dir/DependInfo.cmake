1037a32b52196a742609f3060f143c2ac9ad44cf
Leonard Ramminger <Matographo@gmail.com>
2024-09-28 00:30:58 +0200

    first commit

---
+++ b/CMakeFiles/cpp.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/leodora/Documents/Dev/Nexis/NXPM PM/CPP/src/sqlite3.c" "CMakeFiles/cpp.dir/src/sqlite3.c.o" "gcc" "CMakeFiles/cpp.dir/src/sqlite3.c.o.d"
+  "/home/leodora/Documents/Dev/Nexis/NXPM PM/CPP/src/Downloader.cpp" "CMakeFiles/cpp.dir/src/Downloader.cpp.o" "gcc" "CMakeFiles/cpp.dir/src/Downloader.cpp.o.d"
+  "/home/leodora/Documents/Dev/Nexis/NXPM PM/CPP/src/cpp.cpp" "CMakeFiles/cpp.dir/src/cpp.cpp.o" "gcc" "CMakeFiles/cpp.dir/src/cpp.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
